906411d974c57e3e2f68b3ae860e914168de765c
PavelAF <65919272+PavelAF@users.noreply.github.com>
2024-10-14 03:47:08 +0300

    Update PVE-ASDaC-BASH.sh

--- PVE-ASDaC-BASH.sh
+++ PVE-ASDaC-BASH.sh
@@ -1357,13 +1357,13 @@ function deploy_access_passwd() {
     local nl=$'\n' tab=$'\t'
     local table=''
     case $format_opt in
-        2) table+="<tr><td>Точка подключения к гипервизору (IP или доменное имя:порт)</td><td>Учётная запись для входа в гипервизор (логин | пароль)</td></tr>";;
+        2) table+="<tr><th>Точка подключения к гипервизору (IP или доменное имя:порт)</th><th>Учётная запись для входа в гипервизор (логин | пароль)</th></tr>";;
         4) table+="\"Точка подключения к гипервизору$nl(IP или доменное имя:порт)\";\"Учётная запись для входа в гипервизор$nl(логин | пароль)\"$nl";;
     esac
 
     for stand_num in "${opt_stand_nums[@]}"; do
         [[ "$1" != set ]] && username="${config_base[access_user_name]/\{0\}/$stand_num}@pve" || username=$stand_num
-        [[ $format_opt == 3 ]] && table+="<tr><td>Точка подключения к гипервизору (IP или доменное имя:порт)</td><td>Учётная запись для входа в гипервизор (логин | пароль)</td></tr>"
+        [[ $format_opt == 3 ]] && table+="<tr><th>Точка подключения к гипервизору (IP или доменное имя:порт)</th><th>Учётная запись для входа в гипервизор (логин | пароль)</th></tr>"
         [[ $format_opt == 5 ]] && table+="\"Точка подключения к гипервизору$nl(IP или доменное имя:порт)\";\"Учётная запись для входа в гипервизор$nl(логин | пароль)\"$nl"
 
         local passwd=$(
@@ -1374,11 +1374,11 @@ function deploy_access_passwd() {
         run_cmd /noexit "pvesh set /access/password --userid '$username' --password '$passwd'" || { echo_err "Ошибка: не удалось установить пароль пользователю $username"; exit 1; }
         case $format_opt in
             1) table+="$tab$username | $passwd$nl";;
-            2|3) table+="<tr><td>$pve_url</td><td>$username | $passwd</td></tr>";;
+            2|3) table+="<tr class=\"data\"><td>$pve_url</td><td>$username | $passwd</td></tr>";;
             4|5) table+="\"$pve_url\";\"$username | $passwd\"$nl";;
         esac
     done
-    [[ "$format_opt" == 2 || "$format_opt" == 3 ]] && table="<table style=\"white-space:nowrap\">$table</table>"
+    [[ "$format_opt" == 2 || "$format_opt" == 3 ]] && table="<style>.data{font-family:Consolas;text-align:center}</style><table border="1" style=\"white-space:nowrap\">$table</table>"
     echo $'\n\n'"$c_lred$table$c_null"
 
 }
